0c0a33f23fc309bb1ef255c8f507dc13af2eda5e
Kinza <kinza.fatima.522@gmail.com>
2018-01-26 18:53:09 -0800

    Kinza_Learning_yii2_25/01/2018

---
+++ Kinza/advanced_yii2/backend/modules/testings/Testings.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\modules\testings;
+
+/**
+ * testings module definition class
+ */
+class Testings extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'backend\modules\testings\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
